6e9bd77df81eee9dfe5592e71bc57db7bb14e8e8
nietup <22.jakub@gmail.com>
2016-11-25 23:27:27 +0200

    Arrow generator

---
+++ junction_supergame/Assets/ArrowGenerator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowGenerator : MonoBehaviour {
+    public int ArrowNo;
+    public int ArrowLimit = 3;
+    private Transform Displayed;
+    private int ArrowCount;
+    public ArrayList Arrows = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+        ArrowNo = 0;
+        ArrowCount = 0;
+
+        GameObject spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowUp"),
+                        new Vector3(transform.position.x, transform.position.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+
+        spawned.transform.Translate(new Vector3(0, -1, 0));
+
+        Displayed = spawned.transform;
+    }
+	
+	public void NewArrow()
+    {
+        ArrowNo = Random.Range(0, 4);
+        ArrowCount++;
+
+        if (Arrows.Count >= ArrowLimit)
+        {
+            Destroy(((Transform)Arrows[0]).gameObject);
+            Arrows.Remove(Arrows[0]);
+        }
+
+        Destroy(Displayed.gameObject);
+        GameObject spawned = new GameObject();
+        switch (ArrowNo)
+        {
+            case 0:
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowUp"),
+                        new Vector3(transform.position.x, transform.position.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+
+                break;
+            case 1:
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowDown"),
+                        new Vector3(transform.position.x, transform.position.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                break;
+            case 2:
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowLeft"),
+                        new Vector3(transform.position.x, transform.position.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                break;
+            case 3:
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowRight"),
+                        new Vector3(transform.position.x, transform.position.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                break;
+        }
+        spawned.transform.Translate(new Vector3(0, -1, 0));
+        Displayed = spawned.transform;
+    }
+}
---
+++ junction_supergame/Assets/ArrowPlacer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ArrowPlacer : MonoBehaviour {
+
+    public Transform ArrowGen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.x = (float)Math.Round(2.0*pos.x)/2.0f;
+            pos.y = (float)Math.Round(2.0 * pos.y) / 2.0f;
+
+            int arrow = GetNextArrow();
+            GameObject spawned = new GameObject();
+            switch (arrow)
+            {
+                case 0:
+                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowUp"),
+                        new Vector3(pos.x, pos.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                    break;
+                case 1:
+                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowDown"),
+                        new Vector3(pos.x, pos.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                    break;
+                case 2:
+                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowLeft"),
+                        new Vector3(pos.x, pos.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                    break;
+                case 3:
+                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowRight"),
+                        new Vector3(pos.x, pos.y, 0),
+                        new Quaternion(0f, 0f, 0f, 0f));
+                    break;
+
+            }
+
+            ArrowGen.GetComponent<ArrowGenerator>().Arrows.Add(spawned.transform);
+            Debug.Log(spawned.transform.position);
+
+        }
+    }
+
+    int GetNextArrow()
+    {
+        int res = ArrowGen.GetComponent<ArrowGenerator>().ArrowNo;
+        ArrowGen.GetComponent<ArrowGenerator>().NewArrow();
+        return res;
+    }
+}

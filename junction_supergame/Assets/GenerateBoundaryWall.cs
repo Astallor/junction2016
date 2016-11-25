82a06e97122cb6444c88cecc4ebe0a4e1c1d8b96
Kosut Ondrej <o.kosut@bdasoft.cz>
2016-11-25 22:33:03 +0200

    Create the level with random walls and graves

---
+++ junction_supergame/Assets/GenerateBoundaryWall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateBoundaryWall : MonoBehaviour {
+
+	public Transform wall;
+	public int xBoundary;
+	public int yBoundary;
+
+	// Use this for initialization
+	void Start () {
+		for( int x = 0; x < xBoundary; x++ )
+		{
+			if( x == 0 || x == (xBoundary - 1) )
+				for( int y = 0; y < yBoundary; y++ )
+				{
+					Instantiate( wall, new Vector3( ((float)x/2), ((float) y / 2), 0 ), Quaternion.identity );
+				}
+			else
+			{
+				Instantiate( wall, new Vector3( ((float) x / 2), 0, 0 ), Quaternion.identity );
+				Instantiate( wall, new Vector3( ((float) x / 2), ((float)(yBoundary - 1))/2f, 0 ), Quaternion.identity );
+			}
+				
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ junction_supergame/Assets/GenerateRandomWalls.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System;
+
+public class GenerateRandomWalls : MonoBehaviour {
+
+	public Transform wall;
+	public Transform grave;
+	public int wallAmount;
+	public int graveAmount;
+
+	// Use this for initialization
+	void Start () {
+		int xBoundary = GetComponent<GenerateBoundaryWall>().xBoundary;
+		int yBoundary = GetComponent<GenerateBoundaryWall>().yBoundary;
+
+		instantiateThings( wall, wallAmount, xBoundary, yBoundary );
+		instantiateThings( grave, graveAmount, xBoundary, yBoundary );
+	}
+
+	private void instantiateThings( Transform asset, int amount, int xBoundary, int yBoundary )
+	{
+		for( int x = 0; x < amount; x++ )
+		{
+			int nextX = 0;
+			int nextY = 0;
+
+			while( true )
+			{
+				bool flag = false;
+				nextX = (int) (Random.value * (xBoundary - 2));
+				nextY = (int) (Random.value * (yBoundary - 2));
+				foreach( GameObject ob in GameObject.FindGameObjectsWithTag( "Wall" ) )
+				{
+					if( Vector3.Distance( new Vector3( (float) nextX / 2, (float) nextY / 2, 0 ), ob.transform.position ) == 0 )
+					{
+						Debug.Log( "Distance: " + Vector3.Distance( new Vector3( (float) nextX / 2, (float) nextY / 2, 0 ), ob.transform.position ) );
+						flag = true;
+					}						
+				}
+				if( !flag ) break;
+			}
+			
+			
+
+
+			if( nextX == 0 ) nextX++;
+			if( nextY == 0 ) nextY++;
+
+			Instantiate( asset, new Vector3( (float) nextX / 2, (float) nextY / 2, 0 ), Quaternion.identity );
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

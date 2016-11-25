9f31492f7f1c93f82dda586512e6f44ae54c38ed
nietup <hhh>
2016-11-26 00:30:53 +0200

    arrow collision check

--- junction_supergame/Assets/ArrowPlacer.cs
+++ junction_supergame/Assets/ArrowPlacer.cs
@@ -16,49 +16,46 @@ public class ArrowPlacer : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(pos);
-            pos.x = (float)Math.Round(2.0*pos.x)/2.0f;
-            pos.y = (float)Math.Round(2.0 * pos.y) / 2.0f;
-           /*
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
-            if (Physics2d.ra)
+            if (!Physics2D.Raycast(pos, new Vector3(0, 0, 1), 10))
             {
-                Debug.Log(hit.transform.gameObject.name);
+
+
+
+                pos.x = (float)Math.Round(2.0 * pos.x) / 2.0f;
+                pos.y = (float)Math.Round(2.0 * pos.y) / 2.0f;
+
+
+                int arrow = GetNextArrow();
+                GameObject spawned = new GameObject();
+                switch (arrow)
+                {
+                    case 0:
+                        spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowUp"),
+                            new Vector3(pos.x, pos.y, 0),
+                            new Quaternion(0f, 0f, 0f, 0f));
+                        break;
+                    case 1:
+                        spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowDown"),
+                            new Vector3(pos.x, pos.y, 0),
+                            new Quaternion(0f, 0f, 0f, 0f));
+                        break;
+                    case 2:
+                        spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowLeft"),
+                            new Vector3(pos.x, pos.y, 0),
+                            new Quaternion(0f, 0f, 0f, 0f));
+                        break;
+                    case 3:
+                        spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowRight"),
+                            new Vector3(pos.x, pos.y, 0),
+                            new Quaternion(0f, 0f, 0f, 0f));
+                        break;
+
+                }
+
+                ArrowGen.GetComponent<ArrowGenerator>().Arrows.Add(spawned.transform);
+
             }
-            */
-
-            int arrow = GetNextArrow();
-            GameObject spawned = new GameObject();
-            switch (arrow)
-            {
-                case 0:
-                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowUp"),
-                        new Vector3(pos.x, pos.y, 0),
-                        new Quaternion(0f, 0f, 0f, 0f));
-                    break;
-                case 1:
-                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowDown"),
-                        new Vector3(pos.x, pos.y, 0),
-                        new Quaternion(0f, 0f, 0f, 0f));
-                    break;
-                case 2:
-                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowLeft"),
-                        new Vector3(pos.x, pos.y, 0),
-                        new Quaternion(0f, 0f, 0f, 0f));
-                    break;
-                case 3:
-                    spawned = (GameObject)Instantiate(Resources.Load("Prefabs/ArrowRight"),
-                        new Vector3(pos.x, pos.y, 0),
-                        new Quaternion(0f, 0f, 0f, 0f));
-                    break;
-
-            }
-
-            ArrowGen.GetComponent<ArrowGenerator>().Arrows.Add(spawned.transform);
-            
-
         }
     }
 

b3e747c7635c5c6aeff20b4fd4774d5f9f28337e
Kosut Ondrej <o.kosut@bdasoft.cz>
2016-11-26 16:36:29 +0200

    Add sprites for skeleton directions, first changing

---
+++ junction_supergame/Assets/BodySpriteAnimations.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodySpriteAnimations : MonoBehaviour {
+
+	public Sprite upSprite;
+	public Sprite downSprite;
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void changeSprite(Vector3 direction )
+	{
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if( Vector3.Distance( direction, new Vector3( 0f, 1f, 0f ) ) < 0.1 )
+			sr.sprite = upSprite;
+		else if( Vector3.Distance( direction, new Vector3( 0f, -1f, 0f ) ) < 0.1 )
+			sr.sprite = downSprite;
+		else if( Vector3.Distance( direction, new Vector3( 1f, 0f, 0f ) ) < 0.1 )
+			sr.sprite = rightSprite;
+		else if( Vector3.Distance( direction, new Vector3( -1f, 0f, 0f ) ) < 0.1 )
+			sr.sprite = leftSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
